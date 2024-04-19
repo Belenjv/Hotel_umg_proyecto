7e333e8e1c527a52f27ed399a590b54131e322b4
Astrid Garcia <astridfgarcia02@gmail.com>
2024-04-18 22:36:50 -0600

    Modificaciones connection string

--- hotel_umg_proyecto/Controllers/UsuarioController.cs
+++ hotel_umg_proyecto/Controllers/UsuarioController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
@@ -7,17 +8,15 @@ using System.Net.Http;
 using System.Web.Http;
 using MySql.Data.MySqlClient;
 
-
 namespace hotel_umg_proyecto.Controllers
 {
     public class UsuarioController : ApiController
     {
         private readonly string _connectionString;
-
         public UsuarioController()
         {
             // La cadena de conexión se obtiene desde appsettings.json
-            _connectionString = "Server=localhost;Port=3307;Database=hotel_umg;Uid=root;Pwd=123456789;";
+            _connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
         }
 
         public IHttpActionResult Get()
@@ -27,17 +26,25 @@ namespace hotel_umg_proyecto.Controllers
             // Intenta conectar con la base de datos
             using (var connection = new MySqlConnection(_connectionString))
             {
-                connection.Open();
-
-                // Realiza una operación simple, como leer algunos datos de una tabla
-                string query = "SELECT * FROM usuario"; // Cambia "EjemploTabla" por el nombre de tu tabla
-                MySqlCommand command = new MySqlCommand(query, connection);
-                MySqlDataReader reader = command.ExecuteReader();
-
-                while (reader.Read())
+                try
                 {
-                    datos.Add(reader["id_usuario"].ToString());
+                    connection.Open();
+
+                    // Realiza una operación simple, como leer algunos datos de una tabla
+                    string query = "SELECT * FROM usuario"; // Cambia "EjemploTabla" por el nombre de tu tabla
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    MySqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        datos.Add(reader["id_usuario"].ToString());
+                    }
+                }catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return InternalServerError();
                 }
+                
 
 
             }
